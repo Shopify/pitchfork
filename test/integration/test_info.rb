436bd53e99ed634c9d1131688f8dd1b2f8987ef2
Jean Boussier <jean.boussier@gmail.com>
2023-07-10 13:15:55 +0200

    Stabilize InfoTest

--- test/integration/test_info.rb
+++ test/integration/test_info.rb
@@ -6,13 +6,13 @@ class InfoTest < Pitchfork::IntegrationTest
 
     pid = spawn_server(app: File.join(ROOT, "test/integration/info.ru"), config: <<~CONFIG)
       listen "#{addr}:#{port}"
-      worker_processes 2
+      worker_processes 1
     CONFIG
 
     assert_healthy("http://#{addr}:#{port}")
 
     response = http_get("http://#{addr}:#{port}/")
-    assert_equal "{:workers_count=>2, :live_workers_count=>2}", response.body
+    assert_equal "{:workers_count=>1, :live_workers_count=>1}", response.body
 
     assert_clean_shutdown(pid)
   end
