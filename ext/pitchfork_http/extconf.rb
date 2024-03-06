540ff6993d2cea192b83e55f88b991fb36f1d909
Jean Boussier <jean.boussier@gmail.com>
2024-03-06 13:29:10 -0500

    Fix Ruby 3.4-dev compatibility

--- ext/pitchfork_http/extconf.rb
+++ ext/pitchfork_http/extconf.rb
@@ -3,6 +3,8 @@ require 'mkmf'
 
 have_const("PR_SET_CHILD_SUBREAPER", "sys/prctl.h")
 have_func("rb_enc_interned_str", "ruby.h") # Ruby 3.0+
+have_func("rb_io_descriptor", "ruby.h") # Ruby 3.1+
+
 if RUBY_VERSION.start_with?('3.0.')
   # https://bugs.ruby-lang.org/issues/18772
   $CFLAGS << ' -DRB_ENC_INTERNED_STR_NULL_CHECK=1 '
